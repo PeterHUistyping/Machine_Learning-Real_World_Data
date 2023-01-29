ea6376a8defec187dd4feae19abdb3dfd6f4d114
PeterHUistyping <2447276617@qq.com>
2023-01-29 20:27:47 +0000

    File updated

---
+++ Doc/1/tick1.py
@@ -0,0 +1,70 @@
+from typing import List, Dict
+import os
+from utils.sentiment_detection import read_tokens, load_reviews
+
+
+def read_lexicon(filename: str) -> Dict[str, int]:
+    """
+    Read the lexicon from a given path.
+
+    @param filename: path to file
+    @return: dictionary from word to sentiment (+1 or -1 for positive or negative sentiments respectively).
+    """
+    pass
+
+
+def predict_sentiment(review: List[str], lexicon: Dict[str, int]) -> int:
+    """
+    Given a list of tokens from a tokenized review and a lexicon, determine whether the sentiment of each review in the
+    test set is positive or negative based on whether there are more positive or negative words.
+
+    @param review: list of tokens from tokenized review
+    @param lexicon: dictionary from word to sentiment (+1 or -1 for positive or negative sentiments respectively).
+    @return: calculated sentiment for each review (+1 or -1 for positive or negative sentiments respectively).
+    """
+    pass
+
+
+def accuracy(pred: List[int], true: List[int]) -> float:
+    """
+    Calculate the proportion of predicted sentiments that were correct.
+
+    @param pred: list of calculated sentiment for each review
+    @param true: list of correct sentiment for each review
+    @return: the overall accuracy of the predictions
+    """
+    pass
+
+
+def predict_sentiment_improved(review: List[str], lexicon: Dict[str, int]) -> int:
+    """
+    Use the training data to improve your classifier, perhaps by choosing an offset for the classifier cutoff which
+    works better than 0.
+
+    @param review: list of tokens from tokenized review
+    @param lexicon: dictionary from word to sentiment (+1 or -1 for positive or negative sentiments respectively).
+    @return: calculated sentiment for each review (+1, -1 for positive and negative sentiments, respectively).
+    """
+    pass
+
+
+def main():
+    """
+    Code to check your work locally (run this from the root directory, 'mlrd/')
+    """
+    review_data = load_reviews(os.path.join('data','sentiment_detection', 'reviews'))
+    tokenized_data = [read_tokens(x['filename']) for x in review_data]
+
+    lexicon = read_lexicon(os.path.join('data', 'sentiment_detection', 'sentiment_lexicon'))
+
+    pred1 = [predict_sentiment(t, lexicon) for t in tokenized_data]
+    acc1 = accuracy(pred1, [x['sentiment'] for x in review_data])
+    print(f"Your accuracy: {acc1}")
+
+    pred2 = [predict_sentiment_improved(t, lexicon) for t in tokenized_data]
+    acc2 = accuracy(pred2, [x['sentiment'] for x in review_data])
+    print(f"Your improved accuracy: {acc2}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ Doc/3/tick3.py
@@ -0,0 +1,100 @@
+from utils.sentiment_detection import clean_plot
+from typing import List, Tuple, Callable
+import os
+
+
+def estimate_zipf(token_frequencies_log: List[Tuple[float, float]], token_frequencies: List[Tuple[int, int]]) \
+        -> Callable:
+    """
+    Use the provided least squares algorithm to estimate a line of best fit in the log-log plot of rank against
+    frequency. Weight each word by its frequency to avoid distortion in favour of less common words. Use this to
+    create a function which given a rank can output an expected frequency.
+
+    @param token_frequencies_log: list of tuples of log rank and log frequency for each word
+    @param token_frequencies: list of tuples of rank to frequency for each word used for weighting
+    @return: a function estimating a word's frequency from its rank
+    """
+    pass
+
+
+def count_token_frequencies(dataset_path: str) -> List[Tuple[str, int]]:
+    """
+    For each of the words in the dataset, calculate its frequency within the dataset.
+
+    @param dataset_path: a path to a folder with a list of  reviews
+    @returns: a list of the frequency for each word in the form [(word, frequency), (word, frequency) ...], sorted by
+        frequency in descending order
+    """
+    pass
+
+
+def draw_frequency_ranks(frequencies: List[Tuple[str, int]]) -> None:
+    """
+    Use the provided chart plotting program to plot the most common 10000 word ranks against their frequencies.
+
+    @param frequencies: A sorted list of tuples with the first element being the word and second its frequency
+    """
+    pass
+
+
+def draw_selected_words_ranks(frequencies: List[Tuple[str, int]]) -> None:
+    """
+    Use the chart plotting program to plot your 10 selected words' word frequencies (from Task 1) against their
+    ranks. Plot the Task 1 words on the frequency-rank plot as a separate series on the same plot (i.e., tell the
+    plotter to draw it as an additional graph on top of the graph from above function).
+
+    @param frequencies: A sorted list of tuples with the first element being the word and second its frequency
+    """
+    pass
+
+
+def draw_zipf(frequencies: List[Tuple[str, int]]) -> None:
+    """
+    Use the chart plotting program to plot the logs of your first 10000 word frequencies against the logs of their
+    ranks. Also use your estimation function to plot a line of best fit.
+
+    @param frequencies: A sorted list of tuples with the first element being the word and second its frequency
+    """
+    pass
+
+
+def compute_type_count(dataset_path: str) -> List[Tuple[int, int]]:
+    """
+     Go through the words in the dataset; record the number of unique words against the total number of words for total
+     numbers of words that are powers of 2 (starting at 2^0, until the end of the data-set)
+
+     @param dataset_path: a path to a folder with a list of  reviews
+     @returns: the number of types for every n tokens with n being 2^k in the form [(#tokens, #types), ...]
+    """
+    pass
+
+
+def draw_heap(type_counts: List[Tuple[int, int]]) -> None:
+    """
+    Use the provided chart plotting program to plot the logs of the number of unique words against the logs of the
+    number of total words.
+
+    @param type_counts: the number of types for every n tokens with n being 2^k in the form [(#tokens, #types), ...]
+    """
+    pass
+
+
+def main():
+    """
+    Code to check your work locally (run this from the root directory, 'mlrd/')
+    """
+    frequencies = count_token_frequencies(os.path.join('data', 'sentiment_detection', 'reviews_large', 'reviews'))
+
+    draw_frequency_ranks(frequencies)
+    draw_selected_words_ranks(frequencies)
+
+    clean_plot()
+    draw_zipf(frequencies)
+
+    clean_plot()
+    tokens = compute_type_count(os.path.join('data', 'sentiment_detection', 'reviews_large', 'reviews'))
+    draw_heap(tokens)
+
+
+if __name__ == '__main__':
+    main()
