c7d134235551e61b8e8c59730784ea68ed3be037
PeterHUistyping <2447276617@qq.com>
2023-02-28 01:22:49 +0000

    Social Networks

--- Social_Network/exercises/tick10.py
+++ Social_Network/exercises/tick10.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from typing import Dict, Set
 
@@ -12,7 +13,25 @@ def load_graph(filename: str) -> Dict[int, Set[int]]:
     @return: a dictionary mapping each node (represented by an integer ID) to a set containing all the nodes it is
         connected to (also represented by integer IDs)
     """
-    pass
+    data = dict()
+    with open(filename, encoding='utf-8') as f:
+        content = f.readlines()
+        for line in content:
+            edge = line.strip()
+            ed = edge.split()
+            source = int(ed[0])
+            target = int(ed[1])
+            if source in data:
+                data[source].add(target)
+            else:
+                data[source] = set()
+                data[source].add(target)
+            if target in data:
+                data[target].add(source)
+            else:
+                data[target] = set()
+                data[target].add(source)
+    return data
 
 
 def get_node_degrees(graph: Dict[int, Set[int]]) -> Dict[int, int]:
@@ -22,7 +41,29 @@ def get_node_degrees(graph: Dict[int, Set[int]]) -> Dict[int, int]:
     @param graph: a dictionary mappings each node ID to a set of node IDs it is connected to
     @return: a dictionary mapping each node ID to the degree of the node
     """
-    pass
+    graph_ = graph.copy()
+    for key in graph:
+        graph_[key] = len(graph[key])
+    return graph_
+
+
+def shortest_paths(g, s, t):
+    queue = []
+    paths = []
+    distance = {s: 0}
+    queue.append((s, [s]))
+    while queue:
+        node, path = queue.pop(0)
+        if node == t:
+            return path
+            # paths.append(path)
+            # continue
+        if node in g:
+            for neighbour in g[node]:
+                if neighbour not in distance or distance[neighbour] == distance[node] + 1:
+                    distance[neighbour] = distance[node] + 1
+                    queue.append((neighbour, path + [neighbour]))
+    return paths
 
 
 def get_diameter(graph: Dict[int, Set[int]]) -> int:
@@ -32,12 +73,51 @@ def get_diameter(graph: Dict[int, Set[int]]) -> int:
     @param graph: a dictionary mappings each node ID to a set of node IDs it is connected to
     @return: the length of the longest shortest path between any pair of nodes in the graph
     """
-    pass
+    diameter = 0
+    key = list(graph)
+    len_key = len(key)
+
+    Shortest_paths = [-1] * (len_key+2)
+
+    # for s in range(0, len_key):
+    #     Flag = [False for i in range(len_key)]
+    #     q = []
+    #     Flag[s] = True
+    #     q.append(s)
+    #     Shortest_paths[s][s] = 0
+    #     while q:
+    #         v = q.pop(0)
+    #         if v in graph:
+    #             for neighbour in graph[v]:
+    #                 if not Flag[neighbour]:
+    #                     Flag[neighbour] = True
+    #                     Shortest_paths[s][neighbour] = Shortest_paths[s][v] + 1
+    #                     print(Shortest_paths[s][neighbour],s,neighbour)
+    #                     diameter = max(diameter, Shortest_paths[s][neighbour])
+    #                     q.append(neighbour)
+    # return diameter
+    for s in range(0, len_key):
+        Flag = [False for i in range(len_key+2)]
+        q = []
+        Flag[s] = True
+        q.append(s)
+        Shortest_paths[s] = 0
+        while q:
+            v = q.pop(0)
+            if v in graph:
+                for neighbour in graph[v]:
+                    # print(len(Flag),neighbour)
+                    if not Flag[neighbour]:
+                        Flag[neighbour] = True
+                        Shortest_paths[neighbour] = Shortest_paths[v] + 1
+                        # print(Shortest_paths[neighbour], s, neighbour)
+                        diameter = max(diameter, Shortest_paths[neighbour])
+                        q.append(neighbour)
+    return diameter
 
 
 def main():
     graph = load_graph(os.path.join('data', 'social_networks', 'simple_network.edges'))
-
     degrees = get_node_degrees(graph)
     print(f"Node degrees: {degrees}")
 
